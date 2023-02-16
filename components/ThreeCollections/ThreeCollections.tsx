de94deac76fb5ada4113daf483a614a9defe4832
ismail sevgi <ismailsevgi95@gmail.com>
2023-02-16 14:15:15 +0300

    .

--- components/ThreeCollections/ThreeCollections.tsx
+++ components/ThreeCollections/ThreeCollections.tsx
@@ -33,7 +33,10 @@ function ThreeCollections({}: Props) {
         </div>
       </div>
       <div className={styles.imageCard}>
-        <img src='/imgs/headphone.jpg' className='h-full w-full object-cover' />
+        <img
+          src='https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+          className='h-full w-full object-cover'
+        />
         <div className={styles.ImageWrapper}>
           <div>
             <p className='text-3xl font-semibold text-white'>Headphone</p>
